ac3b324891430ed91a77db47f1991389dca8fbef
irinamarzioni <imarzioni@devartis.com>
2022-10-09 19:24:36 -0300

    correccion

--- classes/herbivoro.ts
+++ classes/herbivoro.ts
@@ -1,11 +1,9 @@
 import { Mamifero } from "./mamifero";
 
 export class Herbivoro extends Mamifero {
-    private age : number;
 
     constructor (name:string, size:string, age:number){
         super(name, size);
-        this.age = age
     }
 
     setFood():void{
